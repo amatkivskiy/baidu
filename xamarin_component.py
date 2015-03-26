6fcfa086f5af998e990b2ff82d53f1b23623786d
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-03-26 13:23:58 +0200

    [dev] Implemented xamarin_component for interaction with xamarin_component.exe.

---
+++ xamarin_component.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+from util import run_command
+
+__author__ = 'maa'
+
+class XamComponent:
+    def __init__(self, xam_component_path):
+        self.xam_component_path = xam_component_path
+        
+        self._validate_component_path()
+
+    def install_component(self, xam_file_path):
+        args = [self.xam_component_path, 'install', xam_file_path]
+
+        run_command(args)
+
+    def clean_xamarin_cache(self, user_directory):
+        path = os.getenv('LOCALAPPDATA') + r"\Xamarin\Cache"
+        print('Deleting folder : ' + path)
+        shutil.rmtree(path)
+
+    def restore_components(self, solution_path):
+        args = [self.xam_component_path, 'restore', solution_path]
+
+        run_command(args)
+
+    def _validate_component_path(self):
+         if not os.path.isfile(self.xam_component_path):
+            raise Exception('Xamarin component exe not found. path = ' + self.xam_component_path)
+         pass
