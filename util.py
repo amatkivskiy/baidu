d04da91ddb0040cd649a5a1325d78d85e061774a
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-03-27 16:39:23 +0200

    [dev] Added functionality for cleaning folder.

--- util.py
+++ util.py
@@ -1,4 +1,6 @@
+import os
 import subprocess
+import shutil
 
 __author__ = 'maa'
 
@@ -21,4 +23,20 @@ def run_command(args, ignoreReturnCode=False):
     elif not ignoreReturnCode:
         raise Exception('Failed to executed command')
 
+    pass
+
+
+def create_or_clean_folder(folder_path):
+    if not os.path.exists(folder_path):
+        print('Creating folder : ' + folder_path)
+        os.makedirs(folder_path)
+    else:
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file)
+
+            if os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+            else:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
     pass
