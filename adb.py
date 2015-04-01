379a3b8b8d431499983ea193f1cdf09e64035a7a
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-04-01 09:33:03 +0300

    [dev] Added script for calling adb from python.

---
+++ adb.py
@@ -0,0 +1,15 @@
+from util import run_command
+
+__author__ = 'maa'
+
+class Adb:
+    def __init__(self, adb_path=None):
+        if adb_path == None:
+            self.adb = 'adb'
+        else:
+            self.adb = adb_path
+
+    def install_apk(self, path_to_apk):
+        params = [self.adb, 'install', '-r', path_to_apk]
+
+        run_command(params)
