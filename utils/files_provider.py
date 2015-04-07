fc105f413e6683980c5d2fcc93a471ebbc9fecba
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-04-07 16:55:21 +0300

    [dev] Change method params because they are not necessary.

--- utils/files_provider.py
+++ utils/files_provider.py
@@ -5,8 +5,8 @@ __author__ = 'maa'
 templates_folder = 'file_templates_folder\\'
 
 
-def create_and_full_fill_file(template_file_name, destination_file_path, file_name, kwargs):
-    template_file = open(templates_folder + template_file_name, 'r')
+def create_and_full_fill_file(template_file_name, destination_file_path, kwargs):
+    template_file = open(template_file_name, 'r')
     file_content = template_file.read()
     template_file.close()
 
@@ -14,6 +14,6 @@ def create_and_full_fill_file(template_file_name, destination_file_path, file_na
 
     final_content = template.substitute(kwargs)
 
-    final_file = open(destination_file_path + '\\' + file_name, 'w')
+    final_file = open(destination_file_path, 'w')
     final_file.write(final_content)
     final_file.close()
