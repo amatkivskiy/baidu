3e89f5e32eafce57ec26187b277f18f4d2c92d75
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-04-15 11:56:34 +0300

    [ADD] Added test project to be able to configure test results.

--- samples/AndroidTestsApp/AndroidTestsApp/MainActivity.cs
+++ samples/AndroidTestsApp/AndroidTestsApp/MainActivity.cs
@@ -3,23 +3,13 @@
 using Android.App;
 using Android.OS;
 using Xamarin.Android.NUnitLite;
+using CustomAndroidTestsConfiguration;
 
 namespace AndroidTestsApp
 {
   [Activity(Label = "AndroidTestsApp", MainLauncher = true)]
-  public class MainActivity : TestSuiteActivity
+  public class MainActivity : ConfigurableTestActivity
   {
-    protected override void OnCreate(Bundle bundle)
-    {
-      // tests can be inside the main assembly
-      AddTest(Assembly.GetExecutingAssembly());
-
-      // or in any reference assemblies
-      // AddTest (typeof (Your.Library.TestClass).Assembly);
-
-      // Once you called base.OnCreate(), you cannot add more assemblies.
-      base.OnCreate(bundle);
-    }
   }
 }
 
